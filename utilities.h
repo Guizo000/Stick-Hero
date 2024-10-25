e23260264569663dba1ef9baf6a08a839d51801e
Guilherme Rocha <guilermerocha.santos@gmail.com>
2024-10-24 22:21:32 -0300

    Opção play do menu feita (n completa)
    -Leve alteração na main e no menu para qnd houver inputs inválidos
    -Adição da função sleep no utilities
    -Função play feita: Input do nome, Seleção de personagem

--- utilities.h
+++ utilities.h
@@ -2,10 +2,13 @@
 #define UTILITIES
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h> 
+#include <time.h>
 
 int checkForNonStdDigits(char string[]);
 int checkForLetters(char string[]);
 int checkForNumbers(char string[]);
-void clearScreen();
+void clearTerminal();
+int sleep(long msec);
 
 #endif 
